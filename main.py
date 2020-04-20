c1e1b8097e8a3349cf8af2e0e5e513ba88828e1b
APIuz <fluctr@mail.ru>
2020-04-21 03:51:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ def up(INPUT):
     requests.get("https://api.telegram.org/bot{}/deleteWebhook".format(INPUT.split(' ')[0])).text
     requests.get("https://api.telegram.org/bot{}/getUpdates?offset=-1".format(INPUT.split(' ')[0])).text
     requests.get("https://api.telegram.org/bot{}/setWebhook?url=https://{}.appspot.com/{}".format(INPUT.split(' ')[0], INPUT.split(' ')[1], (INPUT.split(' ')[0].split(":")[1])[:-20])).text
-threading.Thread(target=up, args=(INPUT,)).start()
 project_id = INPUT.split(' ')[1]
 data = open('app_engine_installer/app_engine_project/app.yaml','r').read()
 data = data.replace('project_nomi', project_id)
@@ -47,6 +46,7 @@ open('app_engine_installer/app_engine_project/main.py','w').write(data)
 os.system('google_appengine/appcfg.py -A '+ project_id + " update app_engine_installer/app_engine_project/app.yaml --noauth_local_webserver")
 try:
     requests.get('https://' + project_id + ".appspot.com/set_webhook").text
+    threading.Thread(target=up, args=(INPUT,)).start()
     print("Agar siz hammasini to'g'ri qilgan bo'lsangiz, bo't ishga tushdi.")
 except Exception as ex:
     print("Server ishlamiyopti. Qandaydir hato bo'lgan. Qaytadan harakat qilinmoqda...")
